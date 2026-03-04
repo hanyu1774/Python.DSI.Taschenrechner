6afd0c02ba25bbcb023439e769016c2cff74abd5
hanyu1774 <60541985+hanyu1774@users.noreply.github.com>
2026-03-04 14:50:41 +0100

    wrote everything into one file

---
+++ taschenrechner.py
@@ -0,0 +1,140 @@
+# ── Models ───────────────────────────────────────────────
+# Hier sind nur reine Daten
+
+# Diese Klasse namens ForegroundColor hat Farbcodes zum
+# Aendern der Schriftfarbe in der Konsole.
+# Diese Farbcodes sind in der "Escape notation", 
+# da sie zur "Escape-Sequenz gehoeren".
+# Die Farbecodes sind hinter dem "\"-Zeichen
+#
+# Was ist eine Escape-Sequenz?
+# Der Befehl "\n" ist z.B. eine Escape-Sequenz, die
+# am Ende eines Strings eine neue Zeile erzeugt
+# Anwendung: 
+# print("Hallo!\nWie geht es dir?")
+#
+
+# Jede Variable der Klasse ForegroundColor, also jede Farbe,
+# hat seinen eigenen Farbcode
+class ForegroundColor:
+    Red: str = "\033[31m"
+    Green: str = "\033[32m"
+    Yellow: str = "\033[33m"
+    Cyan: str = "\033[36m"
+    Orange: str = "\033[38;5;208m"
+    Reset: str = "\033[0m"
+
+# Anwendung - Hier wird die Schriftfarbe des Strings in der print() Funktion
+# auf gelb gesetzt.
+# print(f"{ForegroundColor.Yello}Hallo, wie geht es dir?{ForegroundColor.Reset}")
+# Man muss am Ende des Strings die benutzte Farbe zuerucksetzen, sonst wird die Farbe des gesamten
+# Textes in der Konsole geaendert
+
+dividing_line = 57 * '#'
+header = f"{ForegroundColor.Green}{dividing_line}\nGuten Tag!\nIch bin der beste Taschenrechner!\nIch kann +, -, x, /, //, % und ** berechnen!\n{dividing_line}{ForegroundColor.Reset}"
+footer = f"{ForegroundColor.Orange}{dividing_line}\nProgram wird beendet...\n{dividing_line}{ForegroundColor.Reset}"
+
+operator_map = {
+    "+" :"\t\t\tAddition",
+    "-" :"\t\t\tSubtraktion",
+    "x" :"\t\t\tMultiplikation",
+    "/" :"\t\t\tDivision",
+    "//" :"\t\t\tDivision mit Abrundung",
+    "%" :"\t\t\tRestzahl einer Division bestimmen",
+    "**" :"\t\t\tPotenzieren",
+}
+
+# ── Flows ───────────────────────────────────────────────────────
+# Hier sind Funktionen, die Aufgaben erledigen
+
+def calculate(operator: str, number_a: float, number_b: float):
+    operations = {
+        # Dieses Dictionary umfasst Lambda-Funktionen
+        # Key: die Operatoren
+        # Value: die Funktion (z.B. Addition, Subtraktion, etc.)
+        "+": lambda a,b: a + b,
+        "-": lambda a,b: a - b,
+        "x": lambda a,b: a * b,
+        "/": lambda a,b: a / b,
+        "//": lambda a,b: a // b,
+        "%": lambda a,b: a % b,
+        "**": lambda a,b: a ** b,
+    }
+    return operations[operator](number_a, number_b)
+
+
+def do_another_calculation() -> bool:
+    error_message: str = f"{ForegroundColor.Red}Fehlerhafte Eingabe! Tippe 'j' oder 'n'.\t\t\t{ForegroundColor.Reset}"
+    while True:
+        yes_or_no = input("Noch einmal? (j/n)\t\t\t\t").strip()
+        if yes_or_no not in ('j', 'n'):
+            print(error_message)
+            continue
+        elif yes_or_no == 'j':
+            return True
+        break
+    return False
+
+def get_number(second_input_request: bool = False)-> float:
+    input_number = ""
+    first_input_message = "Tippe die erste Zahl ein... \t\t\t"
+    second_input_message = "Tippe die zweite Zahl ein... \t\t\t"
+    error_message = f"{ForegroundColor.Red}Fehlerhafte Eingabe! Du musst eine Zahl eintippen.{ForegroundColor.Reset}"
+
+    while True:
+        if not second_input_request:
+            input_number = input(first_input_message).strip()
+        else:
+            input_number = input(second_input_message).strip()
+        if not input_number.isdecimal():
+            print(f"{error_message}");
+            continue;
+        break;
+    return float(input_number)
+
+def get_operator():
+    chosen_operator = ""
+    valid_operators = ["+", "-", "x", "/", "//", "%", "**"]
+    error_message = f"{ForegroundColor.Red}Fehlerhafte Eingabe: Bitte gebe einen Operator ein.{ForegroundColor.Reset}"
+    while True:
+        chosen_operator = input("Was willst du mit dieser Zahl tun?\t\t").strip()
+        if chosen_operator not in valid_operators:
+            print(error_message);
+            continue;
+        else:
+            break;
+    return chosen_operator
+
+def get_result_message(first_number: float, operator: str, second_number: float, result: float)-> str:
+    return f"Das Ergebnis der Rechnung {first_number} {operator} {second_number} lautet {result} !"
+
+
+# ── Workflow ─────────────────────────────────────────────────────
+# Orchestrierung der Logik
+
+# Diese Funktion, run_workflow, dient zur Orchestration,
+# um die Hauptlogik des Programmes auszufuehren.
+
+def run_workflow():
+    print(f"{ForegroundColor.Green}{header}{ForegroundColor.Reset}");
+    print(f"{ForegroundColor.Cyan}{dividing_line}\nOperatoren\n{dividing_line}")
+    for key, value in operator_map.items():
+        print(f"{key} {value}")
+    print(f"{dividing_line}{ForegroundColor.Reset}")
+    while True:
+        first_number = get_number()
+        selected_operator = get_operator()
+        second_number = get_number(True)
+        calculation_result = calculate(selected_operator, first_number, second_number)
+        result_message = get_result_message(first_number, selected_operator, second_number, calculation_result) 
+        print(f"{ForegroundColor.Yellow}{dividing_line}\n{result_message}\n{dividing_line}{ForegroundColor.Reset}")
+        another_round: bool = do_another_calculation()
+        if another_round:
+            continue
+        break
+    print(f"{ForegroundColor.Green}{footer}{ForegroundColor.Reset}") 
+
+# ── Ausfuerung des Taschenrechners ────────────────────────────────────────────────────────────────
+
+if __name__ == "__main__":
+    run_workflow()
