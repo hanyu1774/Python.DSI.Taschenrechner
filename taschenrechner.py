c9f61bf9358886e1cdd19c146e446435cba88734
hanyu1774 <60541985+hanyu1774@users.noreply.github.com>
2026-03-04 14:58:56 +0100

    fixed color issue

--- taschenrechner.py
+++ taschenrechner.py
@@ -33,7 +33,7 @@ class ForegroundColor:
 
 dividing_line = 57 * '#'
 header = f"{ForegroundColor.Green}{dividing_line}\nGuten Tag!\nIch bin der beste Taschenrechner!\nIch kann +, -, x, /, //, % und ** berechnen!\n{dividing_line}{ForegroundColor.Reset}"
-footer = f"{ForegroundColor.Orange}{dividing_line}\nProgram wird beendet...\n{dividing_line}{ForegroundColor.Reset}"
+footer = f"{dividing_line}\nProgram wird beendet...\n{dividing_line}"
 
 operator_map = {
     "+" :"\t\t\tAddition",
